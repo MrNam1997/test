c730157871d9fadba9c936e802c17218a00d2e0a
MrNam1997 <nammaco997@gmail.com>
2019-09-05 10:49:11 +0700

    example search dicionalry

---
+++ tuDien/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<head>
+    <style>
+        input[type=text] {
+            width: 300px;
+            font-size: 16px;
+            border: 2px solid #ccc;
+            border-radius: 4px;
+            padding: 12px 10px 12px 10px;
+        }
+
+        #submit {
+            border-radius: 2px;
+            padding: 10px 32px;
+            font-size: 16px;
+        }
+    </style>
+</head>
+<body>
+<h2>Từ Điển Anh - Việt</h2>
+<form method="get">
+    <input type="text" name="search" placeholder="Nhập từ cần tìm"/>
+    <input type="submit" id="submit" value="Tìm"/>
+</form>
+</body>
+</html>
+<?php
+$dictionary = ["hello" => "xin chao", "how" => "the nao", "book" => "quyen vo",
+    "computer" => "may tinh"];
+if ($_SERVER["REQUEST_METHOD"] == "GET");
+$searchword = $_POST["search"];
+$flag = 0;
+foreach ($dictionary as $key => $value) {
+    if ($key == $searchword) {
+        echo "từ " . $key . " có nghĩa là " . $value;
+        echo "<br>";
+        $flag = 1;
+    }
+}
+if ($flag == 0) {
+    echo "không tìm thấy từ cần tra ";
+}
