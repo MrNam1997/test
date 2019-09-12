0d5587221e35868354277f8d896c198826b3105f
MrNam1997 <nammaco997@gmail.com>
2019-09-12 09:16:26 +0700

    bai thuc hanh Abtract and InterFace class Animal and Fruits

---
+++ abtract_interface_class_Animals_and_fruits/index.php
@@ -0,0 +1,20 @@
+<?php
+include('Animal/Chicken.php');
+include('Animal/Tiger.php');
+include('Fruits/Apple.php');
+include('Fruits/Orange.php');
+echo('---- Animals<br>');
+$animals[0] = new Tiger();
+$animals[1] = new Chicken();
+foreach ($animals as $animal) {
+    echo $animal->makeSound() . '<br>';
+    if ($animal instanceof Chicken) {
+        echo $animal->howToEat() . '<br>';
+    }
+}
+echo('---- Fruits<br>');
+$fruits[0] = new Apple();
+$fruits[1] = new Orange();
+foreach ($fruits as $fruit) {
+    echo $fruit->howToEat() . '<br>';
+}
